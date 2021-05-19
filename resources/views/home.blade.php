db5a6d79eb8280c86e0fdc28ec6e8c77f755746b
wvwall <waltervelardo@gmail.com>
2021-05-19 15:20:07 +0200

    Completed + bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,16 @@
 
     @section('main')
       
-       <!--  -->
+    <div class="cards">
+        @foreach ($holidays as $holiday)
+            <div class="card">
+                <h1> Id: {{ $holiday->id }}</h1>
+                <h2> Destination: {{ $holiday->destination }}</h2>
+                <h3> Date: {{ $holiday->date }}</h3>
+                <h4> Number of people: {{ $holiday->n_person }}</h4>
+                <h5>Price: $ {{ $holiday->price }}</h5>
+                <h6>Days: {{ $holiday->days }}</h6>
+            </div>
+        @endforeach
+        </div>
     @endsection
